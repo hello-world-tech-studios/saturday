05ac29c9fc6abf4e0c46b403a10c8b82c202a763
Hack Reactor Student <student@hackreactor.com>
2017-06-24 10:56:52 -0500

    I started the thing and tried to do some extras with letter keys but got side tracked.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  
+  var keys = $("textarea");
+  var bounceDiv = $("#anotherDiv");
+  $(bounceDiv).click(function(){
+    $(bounceDiv).addClass('animated wobble infinite')
+  })
+  $(keys).keypress(function(event){
+    var letter = event.key;
+    if(letter="e"){
+      $(bounceDiv).addClass('animated jello infinite')
+    }
+  })
 })
